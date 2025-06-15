8f834a2ec3b24702bd68e37edaa5a3c09bf5831f
Jules-G-91 <julian.b.goodman@hotmail.co.uk>
2025-06-15 21:26:44 +0100

    Tidied up code

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -5,8 +5,26 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
+const openModal = () => {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
 for (let i = 0; i < btnsOpenModal.length; i++) {
   btnsOpenModal[i].addEventListener('click', () => {
-    modal.classList.remove('hidden');
+    openModal();
   });
 }
+
+btnCloseModal.addEventListener('click', () => {
+  closeModal();
+});
+
+overlay.addEventListener('click', () => {
+  closeModal();
+});
